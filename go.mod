19338bdd2f12130abee2e3d5a95b97cc15a3b3a9
xicu <xicuxabiel@gmail.com>
2025-03-27 22:08:25 +0100

    Websocket is up and running

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xicu/se-backend
 
 go 1.24.1
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- server.go
+++ server.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 type User struct {
@@ -14,11 +17,17 @@ type User struct {
 }
 
 var users []User
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow all origins for simplicity
+	},
+}
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("GET /users/{id}", getUsers)
 	http.HandleFunc("POST /users/{id}", createUser)
+	http.HandleFunc("/socket", socketHandler) // WebSocket endpoint
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -38,4 +47,24 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&newUser)
 	users = append(users, newUser)
 	json.NewEncoder(w).Encode(newUser)
+}
+
+func socketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Fprint(w, "El socket del server haciendo movidas")
+
+	for {
+		timestamp := time.Now().Format(time.RFC3339)
+		err := conn.WriteMessage(websocket.TextMessage, []byte(timestamp))
+		if err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
