c746ec4992f0b4749974dfa997a2dc0c4ed6fb0e
xicu <xicuxabiel@gmail.com>
2025-04-04 19:19:02 +0200

    Nueva fuente y fondo rancier

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Chrono({ time }) {
     const minutes = Math.floor(ms / 60000);
     const seconds = Math.floor((ms % 60000) / 1000);
     const milliseconds = ms % 1000;
-    return `${minutes}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(3, '0')}`;
+    return `${minutes}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(3, '0')}`;
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import App from "./App";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap');
+    </style>
     <App />
   </StrictMode>
 );
